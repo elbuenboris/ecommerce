4a8a4c1941102f8ec72152ffa563407c2afb411b
Marco Romero <borisromero.@gmail.com>
2022-08-10 15:11:53 -0500

    primer formulario

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 class Articles(models.Model):
@@ -5,3 +6,10 @@ class Articles(models.Model):
     description = models.CharField(max_length=1000)
     creation_date = models.DateField(auto_now_add=True)
     author = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
--- products/views.py
+++ products/views.py
@@ -3,9 +3,12 @@ from products.models import Products
 
 
 def create_product(request):
-    new_product = Products.objects.create(name = 'Coca cola 1lt', price = 20, stock = 10)
-    context = {
-        'new_product':new_product
+    productos =Products.objects.all
+    context = {}
+    if len(productos) >= 3:
+        new_product = Products.objects.create(name = 'Coca cola 1lt', price = 20, stock = 10)
+        context = {
+            'new_product':new_product
     }
     return render(request, 'products/new_product.html', context=context)
 
@@ -15,3 +18,6 @@ def list_products(request):
         'products':products
     }
     return render(request, 'products/products_list.html', context=context)
+
+def primer_formulario(request):
+    return render(request, 'products/primer_formulario.html', context={})
