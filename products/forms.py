fdf3f40ade69140d35f7139487ba4c90c0564354
Marco Romero <borisromero.@gmail.com>
2022-08-10 16:27:26 -0500

    form 3

---
+++ products/forms.py
@@ -0,0 +1,8 @@
+from socket import fromshare
+from django import forms
+
+class Formulario_productos(forms.Form):
+    name = forms.CharField(max_length=40)
+    price = forms.FloatField()
+    description = forms.CharField(max_length=200)
+    stock = forms.IntegerField()
--- products/views.py
+++ products/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import Products
-
+from products.forms import Formulario_productos
 
 def create_product(request):
-    productos =Products.objects.all
-    context = {}
-    if len(productos) >= 3:
-        new_product = Products.objects.create(name = 'Coca cola 1lt', price = 20, stock = 10)
-        context = {
-            'new_product':new_product
-    }
-    return render(request, 'products/new_product.html', context=context)
+
+    if request.method == 'POST':
+        form = Formulario_productos(request.POST)
+
+        if form.is_valid():
+            Products.objects.create(
+                name = form.cleaned_data['name'],
+                price = form.cleaned_data['price'],
+                description = form.cleaned_data['description'],
+                stock = form.cleaned_data['stock']
+            )
+            
+            return redirect(list_products)
+
+    elif request.method == 'GET':
+        form = Formulario_productos()
+        context = {'form':form}
+        return render(request, 'products/new_product.html', context=context)
 
 def list_products(request):
     products = Products.objects.all()
@@ -20,4 +30,7 @@ def list_products(request):
     return render(request, 'products/products_list.html', context=context)
 
 def primer_formulario(request):
+    print(request.method)
+    if request.method == 'POST':
+        Products.objects.create(name = request.POST ['name'])
     return render(request, 'products/primer_formulario.html', context={})
